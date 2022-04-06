85f816c8e032e75c0a5f3b2afa116bc459089bfd
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2022-04-06 13:19:44 +0200

    Create grid area in map element

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let welcomeBtn = document.querySelector('.welcomeScreen__btn')
 let welcomeScreen = document.querySelector('.welcomeScreen')
 let descriptionBtn = document.querySelector('.gameDescription__btn')
 let gameDescription = document.querySelector('.gameDescription')
+let xCoordinates = document.querySelector('.gameBoard__xCoordinates')
+let yCoordinates = document.querySelector('.gameBoard__yCoordinates')
+let map = document.querySelector('.gameBoard__map')
 
 function closeWindow(arg) {
     arg.classList.add('visible')
