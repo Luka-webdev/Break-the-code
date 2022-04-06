d865abb3f27adab3cb04273e200908a73dbf1995
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2022-04-06 13:40:01 +0200

    Add function to create boxes in city map

--- script.js
+++ script.js
@@ -2,9 +2,22 @@ let welcomeBtn = document.querySelector('.welcomeScreen__btn')
 let welcomeScreen = document.querySelector('.welcomeScreen')
 let descriptionBtn = document.querySelector('.gameDescription__btn')
 let gameDescription = document.querySelector('.gameDescription')
-let xCoordinates = document.querySelector('.gameBoard__xCoordinates')
-let yCoordinates = document.querySelector('.gameBoard__yCoordinates')
-let map = document.querySelector('.gameBoard__map')
+let city = document.querySelector('.gameBoard__city')
+
+let xCoordinatesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+let yCoordinatesArray = ['A', 'B', 'C', 'D', 'E', 'F']
+let numberOfBoxes = xCoordinatesArray.length * yCoordinatesArray.length
+
+function createCityBoxes() {
+    let counter = 1
+    while (counter <= numberOfBoxes) {
+        let box = document.createElement('div')
+        box.setAttribute('id', counter)
+        box.setAttribute('class', 'gameBoard__cityBox')
+        counter++;
+        city.appendChild(box)
+    }
+}
 
 function closeWindow(arg) {
     arg.classList.add('visible')
@@ -16,3 +29,5 @@ welcomeBtn.addEventListener('click', () => {
 descriptionBtn.addEventListener('click', () => {
     closeWindow(gameDescription)
 })
+
+createCityBoxes()
