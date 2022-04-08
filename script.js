b91619eb02d60737699b2cc7da9a2cfbd495c768
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2022-04-08 09:04:55 +0200

    Add function to control game

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let coordinates = document.querySelector('.gameBoard__target')
 let xCoordinatesArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 let yCoordinatesArray = ['A', 'B', 'C', 'D', 'E', 'F']
 let numberOfBoxes = xCoordinatesArray.length * yCoordinatesArray.length
+let mainCounter = 0;
 
 function createCityBoxes() {
     let counter = 1
@@ -26,16 +27,35 @@ function createCityBoxes() {
         }
         city.appendChild(box)
     }
-
-    startGame()
 }
 
-function startGame() {
-    let num = (Math.floor(Math.random() * numberOfBoxes + 1))
+createCityBoxes()
+choiceEnemyCoordinate()
+
+function choiceEnemyCoordinate() {
+    num = (Math.floor(Math.random() * numberOfBoxes + 1))
     let targetBox = document.getElementById(num)
     coordinates.textContent = targetBox.getAttribute('coordinate')
-
 }
+
+function getEnemyBoxId(item) {
+    if (mainCounter < numberOfBoxes / 2) {
+        boxId = parseInt(item.getAttribute('id'))
+        if (num == boxId) {
+            console.log('ok')
+            choiceEnemyCoordinate()
+        } else {
+            console.log('błąd')
+        }
+        mainCounter++;
+        console.log(mainCounter)
+    }
+}
+
+let boxes = document.querySelectorAll('.gameBoard__cityBox')
+boxes.forEach(box => {
+    box.addEventListener('click', () => getEnemyBoxId(box))
+})
 
 function closeWindow(arg) {
     arg.classList.add('visible')
@@ -46,14 +66,4 @@ welcomeBtn.addEventListener('click', () => {
 })
 descriptionBtn.addEventListener('click', () => {
     closeWindow(gameDescription)
-})
-
-createCityBoxes()
-
-let boxes = document.querySelectorAll('.gameBoard__cityBox')
-boxes.forEach(box => {
-    box.addEventListener('click', (e) => {
-        boxId = parseInt(box.getAttribute('id'))
-        console.log(boxId)
-    })
 })
