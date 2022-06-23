76af119ecf7fb5f173ad6976d38071cde7b56661
Łukasz Pełka <lukasz.pelka.ziip@gmail.com>
2022-06-23 19:53:03 +0200

    Js and css files minimalization

--- cityPlan.js
+++
@@ -1,3 +0,0 @@
-// export function makeCityPlan() {
-//     console.log('ok')
-// }
--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,29 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        autoprefixer: {
-            dist:{
-                src:'css/style.css',
-                dest:'css/styleprefixer.css'
-            }
-          },
-          cssmin: {
-            target: {
-              files: {
-                'css/styleprefixer.min.css': ['css/styleprefixer.css']
-              }
-            }
-          }
-    });
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-  };
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    autoprefixer: {
+      dist: {
+        src: 'css/style.css',
+        dest: 'css/styleprefixer.css'
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          'css/styleprefixer.min.css': ['css/styleprefixer.css']
+        }
+      }
+    },
+    uglify: {
+      target: {
+        files: {
+          'scripts/script.min.js': ['scripts/script.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+};
